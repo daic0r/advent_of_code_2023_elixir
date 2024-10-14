b94e8e5629eefd26d56e4df5e6eb21c83fddbf58
daic0r <daic0r.dev@gmail.com>
2024-10-14 08:20:50 +0200

    Part 1 works

--- day3/lib/day3/part1.ex
+++ day3/lib/day3/part1.ex
@@ -3,18 +3,22 @@ defmodule Day3.Part1 do
   alias Day3.Number
   alias Day3.Symbol
 
-  defp read_lines(file) do
+  def read_lines(file) do
     {:ok, data} = File.read(file)
     data
     |> String.split("\n")
     |> Enum.filter(&(String.length(&1) > 0))
   end
 
-  defp parse_graphemes(<<>>, %{ res: res }) do
+  # Behold, elements are added to res in reverse, so that we can prepend for 
+  # efficiency
+  def parse_graphemes(<<>>, %{ number: number, res: res, idx: _idx }) when number == nil do
     res 
   end
-  
-  defp parse_graphemes(<<ch::utf8, rest::binary>>, %{ number: number, res: res, idx: idx } = state) do
+  def parse_graphemes(<<>>, %{ number: number, res: res, idx: idx }) when number != nil do
+    [ %Number{ number | to: idx-1 } | res ]
+  end
+  def parse_graphemes(<<ch::utf8, rest::binary>>, %{ number: number, res: res, idx: idx } = state) do
     case ch do
       ch when ch in ?0..?9 -> 
         if number == nil do
@@ -27,7 +31,7 @@ defmodule Day3.Part1 do
         end
       ?. ->
         if number do
-          parse_graphemes(rest, %{ state | number: nil, res: res ++ [%Number{ number | to: idx }], idx: idx + 1 })
+          parse_graphemes(rest, %{ state | number: nil, res: [%Number{ number | to: idx-1 } | res], idx: idx + 1 })
         else
           parse_graphemes(rest, %{ state | idx: idx + 1 })
         end
@@ -35,37 +39,37 @@ defmodule Day3.Part1 do
         if number do
           parse_graphemes(rest, %{ state | 
             number: nil,
-            res: res ++ [%Number{ number | to: idx }, %Symbol{ col: idx }],
+            res: [%Symbol{ col: idx }, %Number{ number | to: idx-1 } | res],
             idx: idx + 1
           })
         else
-          parse_graphemes(rest, %{ state | number: nil, res: res ++ [%Symbol{ col: idx }], idx: idx + 1 })
+          parse_graphemes(rest, %{ state | number: nil, res: [%Symbol{ col: idx } | res], idx: idx + 1 })
         end
     end 
   end
 
-  defp parse_line(line) do
+  def parse_line(line) do
     parse_graphemes(line, %{ number: nil, res: [], idx: 0 }) # |> IO.inspect
   end
 
-  defp parse(lines) do
+  def parse(lines) do
     lines 
     |> Enum.map(fn line ->
       parse_line(line)
     end)
   end
 
-  defp is_symbol_adjacent(%Symbol{ col: col }, %Number{ from: from, to: to }) do
-    col >= from && col <= to 
+  # def is_symbol_adjacent(%Symbol{ col: col }, %Number{ from: from, to: to }) do
+  #   col === from-1 or col === to+1
+  # end
+  def is_symbol_adjacent(%Number{ from: from, to: to }, %Symbol{ col: col }) do
+    col === from-1 or col === to+1
   end
-  defp is_symbol_adjacent(%Number{ from: from, to: to }, %Symbol{ col: col }) do
-    col >= from && col <= to 
-  end
-  defp is_symbol_adjacent(_arg1, _arg2) do
+  def is_symbol_adjacent(_arg1, _arg2) do
     false
   end
 
-  defp find_relevant_numbers_inside_line(line) do
+  def find_relevant_numbers_inside_line(line) do
     line
     |> Enum.chunk_every(2, 1) 
     |> Enum.filter(fn
@@ -77,11 +81,12 @@ defmodule Day3.Part1 do
       [%Number{} = num, %Symbol{}] -> num
       [%Symbol{}, %Number{} = num] -> num
     end)
-    |> IO.inspect
+    # |> IO.inspect
   end
 
-  defp find_relevant_numbers([line1 | [line2 | data]], ret) do
-    ret = for num <- find_relevant_numbers_inside_line(line1), into: ret, do: num
+  def find_relevant_numbers([line1 | [line2 | data]], ret, i) do
+    ret = for num <- find_relevant_numbers_inside_line(line1), into: ret, do: num |> IO.inspect
+    IO.puts "\n"
     get_num_from_combo = fn 
         %Number{ from: from, to: to } = num, %Symbol{ col: col } ->
           if col >= from-1 && col <= to+1 do
@@ -93,27 +98,28 @@ defmodule Day3.Part1 do
           end
         _, _ -> nil
     end
-    ret = for i1 <- line1, i2 <- line2, (combo = get_num_from_combo.(i1, i2)) != nil, into: ret, do: combo
-    find_relevant_numbers([line2 | data], ret)  
+    ret = for i1 <- line1, i2 <- line2, (combo = get_num_from_combo.(i1, i2)) != nil, into: ret, do: combo |> IO.inspect
+    IO.puts "------------------------------------------------------------\n"
+    find_relevant_numbers([line2 | data], ret, i + 1)  
   end
-  defp find_relevant_numbers([line1 | data], ret) do
+  def find_relevant_numbers([line1 | data], ret, i) do
     ret = for num <- find_relevant_numbers_inside_line(line1), into: ret, do: num
-    find_relevant_numbers(data, ret)
+    find_relevant_numbers(data, ret, i + 1)
   end
-  defp find_relevant_numbers([], ret) do
+  def find_relevant_numbers([], ret, i) do
     ret 
   end
 
 
-  defp process(file) do
+  def process(file) do
     lines = read_lines(file)
-    data = parse(lines) 
-    numbers = find_relevant_numbers(data, MapSet.new()) |> IO.inspect
+    data = parse(lines)# |> IO.inspect
+    numbers = find_relevant_numbers(data, MapSet.new(), 1)# |> IO.inspect
     numbers |> Enum.reduce(0, fn x, acc -> acc + elem(Integer.parse(x.value), 0) end)
   end
 
   def start(_type, _args) do
-    result = process("input.txt")
+    result = process("input2.txt")
     IO.puts "Result = #{result}"
     {:ok, self()}
   end
--- day3/test/day3/part1_test.exs
+++ day3/test/day3/part1_test.exs
@@ -2,7 +2,42 @@ defmodule Day3.Part1Test do
   use ExUnit.Case
   doctest Day3.Part1
 
-  test "greets the world" do
-    assert Day3.Part1.hello() == :world
+  test "find numbers touching symbols across lines" do
+    line1 = "418..*..57........125...141.........965..382..177.......*.....-.......390.....801....*.....&...659....406....912........614.448............."
+    line2 = "....926...................*.#.........%.................517......*......%........%.301............$........*.......694......$......&........"
+    data = [ Day3.Part1.parse_line(line1), Day3.Part1.parse_line(line2) ]
+
+    ret = Day3.Part1.find_relevant_numbers(data, MapSet.new(), 1)
+    assert ret == MapSet.new([ 
+      %Day3.Number{ from: 4, to: 6, value: "926" },
+      %Day3.Number{ value: "141", from: 24, to: 26 },
+      %Day3.Number{ value: "965", from: 36, to: 38 },
+      %Day3.Number{ value: "517", from: 56, to: 58 },
+      %Day3.Number{ value: "390", from: 70, to: 72 },
+      %Day3.Number{ value: "801", from: 78, to: 80 },
+      %Day3.Number{ value: "301", from: 83, to: 85 },
+      %Day3.Number{ value: "659", from: 95, to: 97 },
+      %Day3.Number{ value: "448", from: 124, to: 126 },
+    ])
+
+
+    line1 = ".......&...625......*.........7*121...........494......=...8......*....@..............................*..........*......998*973.......$....."
+    line2 = "....691............614...795..........152............120...........238..496...........................477..........................994......"
+    data = [ Day3.Part1.parse_line(line1), Day3.Part1.parse_line(line2) ]
+
+    ret = Day3.Part1.find_relevant_numbers(data, MapSet.new(), 1)
+    assert ret == MapSet.new([ 
+      %Day3.Number{ value: "691", from: 4, to: 6 },
+      %Day3.Number{ value: "614", from: 19, to: 21 },
+      %Day3.Number{ value: "7", from: 30, to: 30 },
+      %Day3.Number{ value: "121", from: 32, to: 34 },
+      %Day3.Number{ value: "120", from: 53, to: 55 },
+      %Day3.Number{ value: "238", from: 67, to: 69 },
+      %Day3.Number{ value: "496", from: 72, to: 74 },
+      %Day3.Number{ value: "477", from: 102, to: 104 },
+      %Day3.Number{ value: "998", from: 120, to: 122 },
+      %Day3.Number{ value: "973", from: 124, to: 126 },
+      %Day3.Number{ value: "994", from: 131, to: 133 },
+    ])
   end
 end
